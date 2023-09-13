eac442abc15982b5ec9bfd5b817992eea62977ef
Mr-Kimochu <126611224+Mr-Kimochu@users.noreply.github.com>
2023-09-13 08:26:28 +0300

    Update 1 chatgpt simple.py

--- 1 chatgpt simple.py
+++ 1 chatgpt simple.py
@@ -1,6 +1,6 @@
 import openai
 
-openai.api_key = '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'
+openai.api_key = 'sk-AD5XPcoLCw5gm3GWbdZ8T3BlbkFJAVsLB7hd5jy'
 
 completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": "Give me some ideas for fintech apps I could build for my start up "}])
 print(completion.choices[0].message.content)
